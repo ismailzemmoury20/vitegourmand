341c698bfdc2f06623a43f2ded4e0fa473814336
ismailzemmoury20 <ismail.zamouri3@gmail.com>
2026-07-23 01:07:23 +0200

    Retrait de Docker au profit de Nixpacks et nettoyage du dépôt
    Le déploiement Docker sur Railway posait des problèmes récurrents (conflit
    de MPM Apache). Passage à Nixpacks comme méthode de build. Suppression au
    passage d'un fichier de verrouillage d'outil qui avait été commité par
    erreur, avec ajout au .gitignore pour éviter que ça se reproduise.

--- docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-PORT="${PORT:-80}"
-
-sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/vitegourmand.conf
-
-exec apache2-foreground
